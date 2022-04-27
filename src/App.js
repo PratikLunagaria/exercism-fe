f98e24a902084ac66c76a5bdc71e453c00017a54
Pratik Lunagaria <ramnikbhai_lunagaria@srmuniv.edu.in>
2022-04-27 07:19:56 +0530

    feat: add jumbotron

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import {Navbar} from "./components/navbar";
+import {Jumbotron} from "./components/jumbotron";
 
 function App() {
-	return <div className={"font-Poppins"}><Navbar/></div>;
+	return (
+		<div className={"font-Poppins"}>
+			<Navbar/>
+			<Jumbotron/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import {HeaderBadgeIcon, HeaderPatternIcon} from "../../assets/header";
+
+export const Jumbotron = () => {
+    return(
+		<div className={"flex flex-col h-64 items-center place-content-center"}>
+			<img className={"ml-2 h-16 w-16 inline-flex align-middle"}  alt={"Profile menu"} src={HeaderBadgeIcon}  />
+			<div className={"inline-flex items-center p-4"}>
+				<h1 className={"text-3xl font-extrabold"}> Testimonials I've left </h1>
+				<h3 className={"rounded-full border-2 border-borderL py-0.5 px-2 ml-4"}> 47 </h3>
+			</div>
+			<img className={"ml-2 h-5 w-20 inline-flex align-middle"}  alt={"Profile menu"} src={HeaderPatternIcon}  />
+		</div>
+    )
+}
