b39d754f238ed1598044cd16bd3c94a04723ed7a
Pratik Lunagaria <ramnikbhai_lunagaria@srmuniv.edu.in>
2022-04-29 15:07:46 +0530

    fix(testimonials): add index for assets

---
+++ src/assets/testimonials/index.js
@@ -0,0 +1,3 @@
+export { default as ArrowDownIcon } from './arrow-down-1.svg';
+export { default as LoadingCircleIcon } from './loading-circle.svg';
+export { default as SearchIcon } from './search.svg';
